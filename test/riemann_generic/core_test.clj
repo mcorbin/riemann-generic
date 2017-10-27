a2a5d13f096a40e9204ee3a671fcbd047629f753
mcorbin <corbin.math@gmail.com>
2017-10-27 18:03:40 +0200

    generate lein project

---
+++ test/riemann_generic/core_test.clj
@@ -0,0 +1,7 @@
+(ns riemann-generic.core-test
+  (:require [clojure.test :refer :all]
+            [riemann-generic.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
