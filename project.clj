811b20c4bf3e2cf600f66295b7fe9396c0af5758
mcorbin <corbin.math@gmail.com>
2017-11-24 15:38:44 +0100

    Modify dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [riemann-cond-dt "1.0.1"]]
+  :dependencies [[riemann-cond-dt "1.0.1"]]
   :main ^:skip-aot riemann-generic.core
   :target-path "target/%s"
-  :profiles {:dev {:dependencies [[riemann "0.2.14"]]}})
+  :profiles {:dev {:dependencies [[riemann "0.2.14"]
+                                  [org.clojure/clojure "1.8.0"]]}})
