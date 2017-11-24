60a20f967b81a3e9c59b0e34802ecf858b7d3e6f
mcorbin <corbin.math@gmail.com>
2017-11-24 15:39:02 +0100

    remove outputs, refactor streams

--- src/riemann_generic/core.clj
+++ src/riemann_generic/core.clj
@@ -2,62 +2,63 @@
   (:require [riemann.streams :refer :all]
             [riemann.config :refer :all]
             [riemann.test :refer :all]
-            [riemann-cond-dt.core :refer :all]
-            [riemann-generic.output :as output]
+            [riemann-cond-dt.core :as dt]
             [clojure.tools.logging :refer :all]))
 
+;; (setq clojure-defun-style-default-indent t)
+
 (defn threshold
-  [{:keys [name params output] :as opts}]
-  (where (and (service (:service params))
-              ((:operation params) (:threshold params)))
-    (with :state "critical"
-      (apply sdo (flatten output)))))
-
-(defn threshold-throttle
-  [{:keys [name params output] :as opts}]
-  (where (and (service (:service params))
-           ((:operation params) (:threshold params)))
-    (throttle (:nb-events params) (:duration params)
+  [opts & children]
+  (where (service (:service opts))
+    (where (and (< (:metric event) (:critical opts))
+                (>= (:metric event) (:warning opts)))
+      (with :state "warning"
+        (fn [event]
+          (call-rescue event children))))
+    (where (>= (:metric event) (:critical opts))
       (with :state "critical"
-        (apply sdo (flatten output))))))
+        (fn [event]
+          (call-rescue event children))))))
 
-(defn above-dt
-  [{:keys [name params output] :as opts}]
-  (above (:threshold params) (:duration params)
+(defn threshold-fn
+  [opts & children]
+  (where (and (service (:service opts))
+              ((:operation opts) (:threshold opts)))
     (with :state "critical"
-      (apply sdo (flatten output)))))
+      (fn [event]
+        (call-rescue event children)))))
 
-(defn below-dt
-  [{:keys [name params output] :as opts}]
-  (below (:threshold params) (:duration params)
+(defn above
+  [opts & children]
+  (dt/above (:threshold opts) (:duration opts)
+   (with :state "critical"
+     (fn [event]
+       (call-rescue event children)))))
+
+(defn below
+  [opts & children]
+  (dt/below (:threshold opts) (:duration opts)
     (with :state "critical"
-      (apply sdo (flatten output)))))
+      (fn [event]
+        (call-rescue event children)))))
 
-(defn outside-dt
-  [{:keys [name params output] :as opts}]
-  (outside (:min-threshold params) (:max-threshold params) (:duration params)
+(defn outside
+  [opts & children]
+  (dt/outside (:min-threshold opts) (:max-threshold opts) (:duration opts)
     (with :state "critical"
-      (apply sdo (flatten output)))))
+      (fn [event]
+        (call-rescue event children)))))
 
-(defn between-dt
-  [{:keys [name params output] :as opts}]
-  (between (:min-threshold params) (:max-threshold params) (:duration params)
+(defn between
+  [opts & children]
+  (dt/between (:min-threshold opts) (:max-threshold opts) (:duration opts)
     (with :state "critical"
-      (apply sdo (flatten output)))))
+      (fn [event]
+        (call-rescue event children)))))
 
-(defn critical-dt
-  [{:keys [name params output] :as opts}]
-  (critical (:duration params)
+(defn critical
+  [opts & children]
+  (dt/critical (:duration opts)
     (with :state "critical"
-      (apply sdo (flatten output)))))
-
-;; {:streams [{:name "foo"
-;;             :stream "critical-dt"
-;;             :params {:duration 100}
-;;             :outputs ["slack-foo" "email bar"]
-;;             } ]
-;;  :outputs [{:name "slack-foo"
-;;             :stream "slack-alert"
-;;             :params {:credentials {}
-;;                      :options {}}}]}
-
+      (fn [event]
+        (call-rescue event children)))))
